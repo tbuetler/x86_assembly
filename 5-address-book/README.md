833791f3be6f2a28b840a99fb52aad3a509b0105
Emmanuel Benoist <emmanuel.benoist@bfh.ch>
2023-10-09 07:36:59 +0200

    Modifications made by Claude

--- 5-address-book/README.md
+++ 5-address-book/README.md
@@ -1,16 +1,58 @@
 # 5-address-book Program an Address Book in C
 
 ## Overview and Goal
-The goal of this project is to write a library containing functions to manipulate an address book. The functions are declared in the file 'addr_book_lib.h' . This file contains also the definitions of the structs that are to be used.
+The goal of this project is to write a library containing functions to manipulate an address book. The functions are declared in the file `addr_book_lib.h` . This file contains also the definitions of the structs that are to be used.
 
-You have to implement the functions in the file 'addr_book_lib.c'. In this file you must implement all the functions that are presented. They are tested in the file 'addr_book_test.c'.
+You have to implement the functions in the file `addr_book_lib.c`. In this file __you must implement all the functions__ that are presented. They are tested in the file `addr_book_test.c`.
 
+### The data structure
+
+#### The address book
+
+An address book is built over 3 structures. First the address book itself 
+
+	```
+	struct addr_book {
+		size_t max_size;
+		size_t size;
+		struct addr_book_item* array; 
+	};
+	
+	```
+The addresses within an address book are stored as an array. The size
+of the address book (i.e. The nomber of entries it contains has to be
+stored in the field `size`
+
+#### The address book item 
+
+Then we have a `struct` for each entry:
+
+	struct addr_book_item {
+		char name[ADDR_BOOK_NAME_MAX_LEN];
+		char first_name[ADDR_BOOK_FIRST_NAME_MAX_LEN];
+		struct date birth_date;
+	};
+
+
+Each entry stores the last name (family name), the first name (given
+name) and the the day of birth of a contact. 
+
+Third, the birth day of a contact is stored into a `struct` for itself
+
+	
+	struct addr_book_item {
+		char name[ADDR_BOOK_NAME_MAX_LEN];
+		char first_name[ADDR_BOOK_FIRST_NAME_MAX_LEN];
+		struct date birth_date;
+	};
+	
 
 
 ## Expected Output
 
-You do not have the right to change both files 'addr_book_lib.h' and 'addr_book_test.c'.
+- You do not have the right to change both files `addr_book_lib.h` and `addr_book_test.c`.
 
-You have to implement the functions inside the file 'addr_book_lib.c'.
+- You have to implement the functions inside the file `addr_book_lib.c`.
 
-The tests must all return 0. If it is not the case, the test will not work.
+- The tests must all return 0. If a test returns another value, it
+means that the test failed.
--- 5-address-book/addr_book_lib.h
+++ 5-address-book/addr_book_lib.h
@@ -1,8 +1,11 @@
 ////////////////////////////////////////////////////////////////
 // Define all the constants for the program
 ///////////////////////////////////////////////////////////////
-#define ADDR_BOOK_INT_SIZE  64
+// Initial Size of the address book
+#define ADDR_BOOK_INIT_SIZE  64
+// Maximal length of a name 
 #define ADDR_BOOK_NAME_MAX_LEN  40
+// Maximal length of the first name
 #define ADDR_BOOK_FIRST_NAME_MAX_LEN  40
 
 
@@ -22,8 +25,8 @@ struct addr_book_item {
 };
 
 struct addr_book {
-    size_t max_size;
-    size_t size;
+    size_t max_size;      // max available size for entries
+    size_t size;          // the number of entries stored in the address book
     struct addr_book_item* array;  
 };
 
@@ -31,35 +34,63 @@ struct addr_book {
 ////////////////////////////////////////////////////////////////////////////////
 // Declare all functions (in header file)
 ///////////////////////////////////////
-// create and empty address book (that contains nobody)
+/** create and empty address book (that contains nobody)
+   - the initial maximum size is defined by the constant 'ADDR_BOOK_INIT_SIZE'
+   that is, at the creation of the address boook, this amount of size should be reserved
+
+   - when the address boook is full, the reserved size maybe extended (normally doubled)
+   by function addr_book_add_item()
+
+*/
 struct addr_book * addr_book_create_empty(void);
 
 // delete and existing list
 void addr_book_delete(struct addr_book* ab);
 
-// insert a person at the end of the list
+
+/** add an entry at the end of the list
+ */
 int addr_book_add_item(struct addr_book* ab, const char* name,const char* first_name, const char* date);
 
-// Returns the number of persons in the address book
+/** Returns the effective number of entries in the address book
+ */
 size_t addr_book_size(const struct addr_book* ab);
 
 // get a pointer to the element number 'index'
 struct addr_book_item* addr_book_get_element_at(struct addr_book* ab,size_t index);
 
-// Remove the element with number 'index'
+
+/** Remove the element with number 'index'. The index is then 'compacted', that is, elements
+    placed after the deleted element are shifted
+ */
 int addr_book_remove_element_at(struct addr_book* ad,size_t index);
 
 // Remove all the elements with a given name 
 int addr_book_remove_element_with_name(struct addr_book* ab,const char* name);
 
-// Print out all the elements (in the form of CSV with semicolon as a separator)
-// output is written in a stream that can be stdout or a file.
+
+/** Print out all the elements (in the form of CSV with semicolon as a separator)
+    to the given stream.
+
+    The stream may be the standard output of the program or a file.
+
+*/
 void addr_book_print(FILE * stream, const struct addr_book* ab);
 
-// save the list in a CSV file
+
+/** Explicitely save the address book in a CSV file (each entry -> one line).
+    - the parameter 'filename' may include a path. We consider that the user
+    has write access to this place.
+ */
 int addr_book_save(const char* filename,const struct addr_book* ab);
 
-// read the list from a CSV file
+
+
+/** read a CSV file to populate the address book. 
+    - each line has the same structure as shown in file that is:
+    <family name>;<first name>;<year>;<month>;<day>, and should be parsed with a
+    regular expression like: "%[^;];%[^;];%d;%hhd;%hhd\n"
+ */
 struct addr_book* addr_book_create_from_file(const char* filename);
 
 // get a new list with all the elements with a given name (new copy)
