5890843cb2083647f79027c70035ee9aaa426d3e
Bütler Tim Philipp <butlt2@bfh.ch>
2023-10-10 22:47:34 +0200

    solution for task 1

--- 1-hello/hello.asm
+++ 1-hello/hello.asm
@@ -1,4 +1,15 @@
+;	Executable name	:	1-hello
+;	Version			:	1.0
+;	Created date	:	10.10.2023
+;	Last update		:	10.10.2023
+;	Author			:	Tim Bütler
+; 	Description		:	A simple program to print "Hello world!"
+;
+
 SECTION .data           ; Section containing initialised data
+
+	HelloMsg: db "Hello world!", 10	;	Text that should be printed
+	HelloLen: equ $-HelloMsg		;	Compute the length of the string
 
 SECTION .bss            ; Section containing uninitialized data
 
@@ -9,6 +20,13 @@ global _start           ; Linker needs this to find the entry point!
 _start:
 
     ;; Add *your code* here to make this program print "Hello world"!
+
+	mov rax, 1
+	mov rdi, 1
+
+	mov rsi, HelloMsg
+	mov rdx, HelloLen
+	syscall
 
     mov rax, 60         ; Code for exit
     mov rdi, 0          ; Return a code of zero
