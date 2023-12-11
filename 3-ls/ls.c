b8699b31a4c67012808f67b8cba4a61c3cbb0ea5
Bütler Tim Philipp <butlt2@bfh.ch>
2023-12-11 18:10:11 +0100

    new version

--- 3-ls/ls.c
+++ 3-ls/ls.c
@@ -2,8 +2,91 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <string.h>
 
-void listDirectory();
+// Function to compare strings for sorting
+int compareStrings(const void *a, const void *b) {
+    return strcmp(*(const char **)a, *(const char **)b);
+}
+
+void listDirectory(const char *path) {
+    DIR *dir;
+    struct dirent *entry;
+    struct stat info;
+    char **entries;
+    int numEntries = 0;
+
+    dir = opendir(path);
+
+    if (dir == NULL) {
+        perror("opendir");
+        exit(EXIT_FAILURE);
+    }
+
+    // Count the number of entries (excluding "." and ".." and those starting with a dot)
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 &&
+            entry->d_name[0] != '.') {
+            numEntries++;
+        }
+    }
+
+    // Allocate memory for entries
+    entries = (char **)malloc(numEntries * sizeof(char *));
+    if (entries == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    rewinddir(dir);  // Reset directory stream position
+
+    // Store entries in the array
+    int index = 0;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 &&
+            entry->d_name[0] != '.') {
+            entries[index] = strdup(entry->d_name);
+            index++;
+        }
+    }
+
+    closedir(dir);
+
+    // Sort the entries
+    qsort(entries, numEntries, sizeof(char *), compareStrings);
+
+    // Print the sorted entries
+    for (int i = 0; i < numEntries; i++) {
+        char fullpath[PATH_MAX];
+        snprintf(fullpath, sizeof(fullpath), "%s/%s", path, entries[i]);
+
+        if (stat(fullpath, &info) != 0) {
+            perror("stat");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("%s", entries[i]);
+
+        // Append / for directories
+        if (S_ISDIR(info.st_mode)) {
+            printf("/");
+        }
+        // Append * for executable files
+        else if (info.st_mode & S_IXUSR) {
+            printf("*");
+        }
+        // Append @ for symbolic links
+        else if (S_ISLNK(info.st_mode)) {
+            printf("@");
+        }
+
+        printf("\n");
+
+        free(entries[i]);  // Free dynamically allocated memory
+    }
+
+    free(entries);
+}
 
 int main(int argc, char *argv[]) {
     const char *path;
@@ -23,46 +106,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
-void listDirectory(const char *path) {
-    DIR *dir;
-    struct dirent *entry;
-    struct stat info;
-
-    dir = opendir(path);
-
-    if (dir == NULL) {
-        perror("opendir");
-        exit(EXIT_FAILURE);
-    }
-
-    while ((entry = readdir(dir)) != NULL) {
-        char fullpath[PATH_MAX];
-        snprintf(fullpath, sizeof(fullpath), "%s/%s", path, entry->d_name);
-
-        if (stat(fullpath, &info) != 0) {
-            perror("stat");
-            exit(EXIT_FAILURE);
-        }
-
-        printf("%s", entry->d_name);
-
-        // Append / for directories
-        if (S_ISDIR(info.st_mode)) {
-            printf("/");
-        }
-        // Append * for executable files
-        else if (info.st_mode & S_IXUSR) {
-            printf("*");
-        }
-        // Append @ for symbolic links
-        else if (S_ISLNK(info.st_mode)) {
-            printf("@");
-        }
-
-        printf("\n");
-    }
-
-    closedir(dir);
-}
-
